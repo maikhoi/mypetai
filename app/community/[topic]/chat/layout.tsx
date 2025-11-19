496f29b9dd2853ae2936dbefe05d54460b3e4d94
Khoi <maikhoi@gmail.com>
2025-11-19 16:28:32 +1100

    1 more try og:image

--- app/community/[topic]/chat/layout.tsx
+++ app/community/[topic]/chat/layout.tsx
@@ -1,4 +1,7 @@
+  // SERVER COMPONENT – REQUIRED FOR METADATA TO WORK
+
+export const dynamic = "force-dynamic";
+
 export default function ChatLayout({ children }: { children: React.ReactNode }) {
-    return <>{children}</>;
-  }
-  
+  return <>{children}</>;
+}
