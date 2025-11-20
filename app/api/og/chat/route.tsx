d0b9d2c2494e54e650350bb49d97d6cf84fe56be
Khoi <maikhoi@gmail.com>
2025-11-21 09:15:23 +1100

    remove div text  due to invalid content type

--- app/api/og/chat/route.tsx
+++ app/api/og/chat/route.tsx
@@ -38,41 +38,24 @@ export async function GET(req: Request) {
     return new ImageResponse(
       (
         <div
-      style={{
-        width: "1200px",
-        height: "630px",
-        background: "#ffffff",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "flex-start",
-        padding: "40px",
-        fontSize: "48px",
-        fontFamily: "Arial",
-      }}
-    >
-      {/* Image */}
-      <img
-        src={imgBuffer as unknown as string}
-        style={{
-          width: "100%",
-          height: "400px",
-          objectFit: "contain",
-        }}
-      />
-
-      {/* Message text */}
-      <div
-        style={{
-          marginTop: "30px",
-          color: "#333",
-          whiteSpace: "pre-wrap",
-          fontSize: "40px",
-          textAlign: "center",
-        }}
-      >
-        {msg?.text ?? "Chat Message from MyPetAI"}
-      </div>
-    </div>
+          style={{
+            width: "1200px",
+            height: "630px",
+            background: "#ffffff",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <img
+            src={imgBuffer as unknown as string}
+            style={{
+              width: "100%",
+              height: "100%",
+              objectFit: "contain",
+            }}
+          />
+        </div>
       ),
       { width: 1200, height: 630 }
     );
